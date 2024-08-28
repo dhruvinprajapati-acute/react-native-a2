da601d57994c5b1c0f7324b510247d995c317afd
Dhruvin Prajapati <dhruvin.prajapati@acuteinformatics.in>
2024-08-28 10:15:58 +0530

    design gameover screen

--- src/screens/GameOverScreen.tsx
+++ src/screens/GameOverScreen.tsx
@@ -1,9 +1,38 @@
-import { Text } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+import Title from "../components/ui/Title";
+import Colors from "../constants/colors";
 
 function GameOverScreen(){
     return(
-        <Text>GameOver Screen</Text>
+        <View style={style.rootContiner}>
+            <Title>Game Over</Title>
+            <View style={style.imageContainer}>
+                <Image style={style.image} source={require('../../assets/images/success.png')} />
+            </View>
+            <Text>Your Phone needed X rounds to gusse the number Y</Text>
+        </View>
     )
 }
+
+const style = StyleSheet.create({
+    rootContiner:{
+        padding:24,
+        justifyContent:'center',
+        alignItems: 'center'
+    },
+    imageContainer:{
+        borderRadius: 130,
+        width:260,
+        height:260,
+        borderWidth:3,
+        borderColor: Colors.primary800,
+        overflow: 'hidden',
+        margin:36,
+    },
+    image:{
+        width:'100%',
+        height: '100%'
+    }
+});
 
 export default GameOverScreen;
